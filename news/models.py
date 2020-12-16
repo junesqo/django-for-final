7bcbbaaec58724c8bddb733d58aa919bc2b96439
junesqo <junesqo@gmail.com>
2020-12-16 22:31:39 +0600

    nice frontend update

--- news/models.py
+++ news/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Articles(models.Model):
     title = models.CharField('Заголовок', max_length=100)
     full_text = models.TextField('Текст записи', max_length=8000)
-    date = models.DateTimeField('Дата публикации')
+    date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
     def __str__(self):
         return self.title
